5ab23add7f34c7de504500d1f626c614d9bdf18e
Ashher Shahzad <AshherShahzad@gmail.com>
2025-01-21 16:20:31 +0500

    Add files via upload

---
+++ FileCopyAutomationTool/Models/FileCopyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FileCopyAutomationTool.Models
+{
+    public class FileCopyModel
+    {
+        public string SourcePath { get; set; } = string.Empty; // Default value to avoid null
+        public string DestinationPath { get; set; } = string.Empty; // Default value
+        public string ResultMessage { get; set; } = string.Empty; // Default value
+    }
+
+}
